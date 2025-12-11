8711646aeb11f221c3080c5a8b3c1a6e71bf37ba
SomberNight <somber.night@protonmail.com>
2025-12-11 16:58:10 +0000

    prepare release 0.1.0

--- src/electrum_aionostr/__init__.py
+++ src/electrum_aionostr/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for aionostr."""
 
 __author__ = """The Electrum Developers"""
-__version__ = '0.0.11'
+__version__ = '0.1.0'
 
 import time
 from typing import Optional, List, Any
